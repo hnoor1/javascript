f4270fc0513cafe7af3a61247c095e5dfc6756a0
hnoor1 <hnoor@hkhk.edu.ee>
2026-09-10 10:04:14 +0300

    Add files via upload

---
+++ h04.js
@@ -0,0 +1,19 @@
+const products = ["Õunad", "Piim", "Leib", "Juust", "Tomatid", "Kanafilee", "Muna", "Sibul", "Apelsinid", "Riis", "Jogurt", "Kartul", "Kalafilee", "Pasta", "Jogurtijook", "Porgandid", "Virsikud", "Pähklid", "Rosinad", "Kapsas", "Kreeka jogurt", "Veiseliha", "Banaanid", "Oliivid", "Mandlid", "Magus kartul", "Greibid"];
+
+
+//console.log(products[0]);
+
+//for
+// for (let i = 0; i < products.length; i++) {
+//     const prod = products[i];
+//     console.log(prod);   
+// }
+
+//while
+let j = 0
+
+while (j < products.lenght) {
+    console.log(products[j]);
+    j++
+}
+//foreach
